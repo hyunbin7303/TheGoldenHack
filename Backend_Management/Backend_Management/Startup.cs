fcce71ba7287087e5aa3befe96f6809c9a3330cb
kevin <hyunbin7303@gmail.com>
2019-10-06 12:30:58 -0400

    Final with UI

--- Backend_Management/Backend_Management/Startup.cs
+++ Backend_Management/Backend_Management/Startup.cs
@@ -42,7 +42,7 @@ namespace Backend_Management
 
             app.UseRouting();
 
-            app.UseAuthorization();
+          //  app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
